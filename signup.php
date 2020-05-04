47531b6b5d614165d2f3167d9aacb24bb0de5fd9
Viyzed <1407637@rgu.ac.uk>
2020-05-04 18:47:48 +0000

    Create signup form

---
+++ signup.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+	<title>User Signup</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+
+<body>
+	<div class="header">
+		<h2>Sign Up</h2>
+	</div>
+</body>
+
+</html>
+
